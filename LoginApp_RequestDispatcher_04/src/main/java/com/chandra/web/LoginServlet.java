48fb1594ede932be50d02c4604d923923d783dfb
Chandu <67627909+chanduonline@users.noreply.github.com>
2020-07-11 08:11:18 +0530

    Add files via upload

---
+++ LoginApp_RequestDispatcher_04/src/main/java/com/chandra/web/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.chandra.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		String uname = request.getParameter("uname");
+		String pwd = request.getParameter("pass");
+		if(uname.equals(pwd)){
+			out.println("<center><h1><u>WELCOME TO OUR WEBSITE</u></h1></center>");
+		}else{
+			out.println("Invalid UserName / PassWord.Please try again");
+			RequestDispatcher rd = request.getRequestDispatcher("./login.html");
+			rd.include(request, response);
+		}
+	}
+
+}
